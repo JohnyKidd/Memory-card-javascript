ae01f895de0884efe320ea30e102466bafa3f425
JohnyKidd <enyedi.tibor1996@gmail.com>
2021-11-17 16:52:00 +0100

    colors added to cards - next step is to shuffle the array

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
-//selecting the cards
+// create the playfield and the cards
+const cardNumber = 12
+
+const playArea = document.querySelector(".cardGrid")
+
+for (let i = 0; i < cardNumber; i++){
+    const card = document.createElement("div")
+    card.className="card"
+    playArea.appendChild(card)
+}
+
+//selecting the cards for further actions
 const deck = document.querySelectorAll('.card')
+
+//color palette
+const colors = ["red", "blue", "green", "purple", "black", "yellow","red", "blue", "green", "purple", "black", "yellow"]
+
+console.log(deck)
 
 //iterate the cards for game logic and animation
 for (let i = 0; i < deck.length; i++){
     deck[i].addEventListener("click", ()=>{
         deck[i].className = "cardClick"
-        console.log("clicked")
-    })
-    deck[i].addEventListener("mouseleave", ()=>{
-        deck[i].className = "card"
-        console.log("left")
+        deck[i].style.background = colors[i]
     })
 }
